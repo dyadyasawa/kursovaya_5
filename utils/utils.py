0ca7567a3a6926e8f70f49b70c428ad39aa89af4
dyadyasawa <dyadyasawa@gmail.com>
2024-02-19 19:38:29 +0300

    Правки в utils/utils.py.

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 
 import psycopg2
 from config import config
+from classes.classes import HeadHunterApi
 
 
 def create_database(name_db):
@@ -45,6 +46,37 @@ def create_tables(name_db):
     cur.close()
     conn.close()
 
+def add_data_to_tables(name_db):
+    """ Функция добавляет информацию в таблицы базы данных. """
+
+    hh = HeadHunterApi()
+    employers = hh.choise_data_for_vacancies()[0]
+    vacancies = hh.choise_data_for_vacancies()[1]
+
+    conn = psycopg2.connect(dbname=name_db, **config())
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    for employer in employers:
+        cur.execute('INSERT INTO employers VALUES(%s, %s, %s),'
+                   f'{employer['employer_id']},'
+                   f'{employer['employer_name']},'
+                   f'{employer['vacancies']}')
+
+    for vacancy in vacancies:
+        cur.execute('INSERT INTO vacancies VALUES(%s, %s, %s, %s, %s, %s),'
+                    f'{vacancy['employer_id']},'
+                    f'{vacancy['employer_name']},'
+                    f'{vacancy['area']},'
+                    f'{vacancy['salary_from']},'
+                    f'{vacancy['salary_to']},'
+                    f'{vacancy['url']},'
+                    f'{vacancy['employer']}')
+
+    cur.close()
+    conn.close()
+
 
 create_database('new_database')
 create_tables('new_database')
+add_data_to_tables('new_database')
