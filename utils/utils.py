1abe21c9a2e78ad12d5159532cf5641a4778f27f
dyadyasawa <dyadyasawa@gmail.com>
2024-02-19 14:23:21 +0300

    Правки в utils/utils.py.

--- utils/utils.py
+++ utils/utils.py
@@ -19,22 +19,31 @@ def create_database(name_db):
 def create_tables(name_db):
     """ Функция для создания таблиц в базе данных. """
 
-    conn = psycopg2.connect(dbname='postgres', **config())
-    with conn:
-        with conn.cursor() as cur:
-            cur.execute('CREATE TABLE employers'
-                        '('
-                        'employer_id  int PRIMARY KEY,'
-                        'employer_name varchar(255) UNIQUE NOT NULL,'
-                        'open_vacancies int)')
+    conn = psycopg2.connect(dbname=name_db, **config())
+    conn.autocommit = True
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE employers'
+                '('
+                'employer_id  int PRIMARY KEY,'
+                'employer_name varchar(255) UNIQUE NOT NULL,'
+                'open_vacancies int'
+                ')')
 
-            cur.execute('CREATE TABLE vacancies'
-                        '('
-                        'employer_id int PRIMARY KEY,'
-                        'employer_name varchar(255) UNIQUE NOT NULL,'
-                        'area varchar(255),'
-                        'salary_from int,'
-                        'salary_to int,'
-                        'url varchar(255),'
-                        'employer int REFERENCES employers(employer_id) NOT NULL)')
+    cur.execute('CREATE TABLE vacancies'
+                '('
+                'employer_id int PRIMARY KEY,'
+                'employer_name varchar(255) UNIQUE NOT NULL,'
+                'area varchar(255),'
+                'salary_from int,'
+                'salary_to int,'
+                'url varchar(255)'
+                ')')
+                # 'employer int REFERENCES employers(employer_id) NOT NULL,'
 
+
+    cur.close()
+    conn.close()
+
+
+create_database('new_database')
+create_tables('new_database')
